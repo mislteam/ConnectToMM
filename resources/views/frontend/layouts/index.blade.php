0a62d82ab1b23cfd5470a26e204ab82bf935e6a5
Chaw <--add>
2026-07-16 15:08:49 +0630

    fix css file time

--- resources/views/frontend/layouts/index.blade.php
+++ resources/views/frontend/layouts/index.blade.php
@@ -120,8 +120,12 @@
     <link href="{{ asset('assets/css/style.css') }}?v{{ filemtime(public_path('assets/css/style.css')) }}"
         rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/css/custom-style.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/css/special-classes.css') }}" type="text/css">
+    <link rel="stylesheet"
+        href="{{ asset('assets/css/custom-style.css') }}?v={{ filemtime(public_path('assets/css/custom-style.css')) }}"
+        type="text/css">
+    <link rel="stylesheet"
+        href="{{ asset('assets/css/special-classes.css') }}?v={{ filemtime(public_path('assets/css/special-classes.css')) }}"
+        type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
